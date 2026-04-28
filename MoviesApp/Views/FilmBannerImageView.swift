0f5cda359542f9fbcebc9727f7d6c59a79453a44
Damitha Raveendra <deamonsarea52@gmail.com>
2026-04-28 23:59:19 +0530

    Image view added

---
+++ MoviesApp/Views/FilmBannerImageView.swift
@@ -0,0 +1,18 @@
+//
+//  FilmBannerImageView.swift
+//  MoviesApp
+//
+//  Created by Damitha Raveendra on 2026-04-28.
+//
+
+import SwiftUI
+
+struct FilmBannerImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilmBannerImageView()
+}
